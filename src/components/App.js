2ceddeef80a285dcb6de465cc6d974f2769bbd29
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-20 13:24:36 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [isLoading, setIsLoading] = useState(true)
+    const [images, setImages] = useState("")
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then((data) => {
+            setImages(data.message);
+            setIsLoading(false)
+          });
+      }, []);
+
+      if (isLoading) return <h3>Loading...</h3>;
+    return(
+        <div>
+            <img src={images} alt=""/>
+
+        </div>
+    );
+}
+
+export default App;
